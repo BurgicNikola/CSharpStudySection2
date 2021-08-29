ac2a43372efd4ba8d1d7a1b9fcd2de48a8ec24d9
BurgicNikola <burgicnikola@gmail.com>
2021-08-29 22:46:18 +0200

    Adding User Input

--- Lesson05UserInput/Program.cs
+++ Lesson05UserInput/Program.cs
@@ -10,6 +10,21 @@ namespace Lesson05UserInput
     {
         static void Main(string[] args)
         {
+            Calculate();
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Prompts the user to enter two numbers and prints their sum to the console.
+        /// </summary>
+        public static void Calculate()
+        {
+            Console.WriteLine("This program prints the sum of the two numbers that you enter.");
+            Console.Write("Enter the first number: ");
+            string num1 = Console.ReadLine();
+            Console.Write("Enter the second number: ");
+            string num2 = Console.ReadLine();
+            Console.WriteLine("The sum of the two numbers that you entered is " + (int.Parse(num1) + int.Parse(num2)) + ".");
         }
     }
 }
