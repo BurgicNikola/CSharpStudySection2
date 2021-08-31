8343fc5bf0fe631d9b60fae44f9897a7bef474d6
Nikola Burgic <nikola.burgic@wm.rs>
2021-09-01 00:26:24 +0200

    Adding Operators

---
+++ Lesson07Operators/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lesson07Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
+
+            // Unary operator
+            num3 = -num1;
+            Console.WriteLine($"num3 is {num3}");
+
+            bool isSunny = true;
+            Console.WriteLine($"Is it sunny? {!isSunny}");
+
+            // Increment operators
+            int num = 0;
+            num++;
+            Console.WriteLine("num is {0}", num);
+            // Post increment
+            Console.WriteLine("num is {0}", num++);
+            // Pre increment
+            Console.WriteLine("num is {0}", ++num);
+
+            // Decrement operators
+            num--;
+            Console.WriteLine("num is {0}", num);
+            // Post decrement
+            Console.WriteLine("num is {0}", num--);
+            // Pre decrement
+            Console.WriteLine("num is {0}", --num);
+
+
+            Console.Read();
+        }
+    }
+}
